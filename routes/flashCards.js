1d28477c48f45a137a4593dd7c14e98249836d14
hernan r <hobbit.rodrigiu@gmail.com>
2021-03-13 04:06:30 -0300

    add fix

--- routes/flashCards.js
+++ routes/flashCards.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 const { poolPromise } = require('../db');
 
+// I will add a login process later
 router.get('/', async (req, res) => {
     try {
         const client = await poolPromise
         const data = await client.query("SELECT * FROM users WHERE id='1'");
-        res.setHeader("Content-Type", "application/json")
-        res.json(data.rows[0]);
+        // res.setHeader("Content-Type", "application/json")
+        const user = data.rows[0];
+        res.render('pages/flashCards', { user } );
     } catch(e) {
         console.log(e)
     }

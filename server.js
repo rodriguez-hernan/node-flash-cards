57640f768ad54f31e3ccdb8d127071ec26703938
hernan r <hobbit.rodrigiu@gmail.com>
2021-03-27 01:13:47 -0300

    add login

--- server.js
+++ server.js
@@ -16,13 +16,57 @@ app.set('port', (process.env.PORT || 5000));
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(session({
+      saveUninitialized: false,
+      resave: false,
+      secret: 'secret-mouse'
+    }))
   .set('view engine', 'ejs');
 
+// middleware
+const logRequest = (req, res, next)=>{
+  console.log("Received a request for: " + req.url);
+  next();
+}
+
 // routes
+app.use(logRequest);
 app.use('/flashCards', flashCards);
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.post('/login', (req, res)=> {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  pool.connect((err, client, done) => {
+    if (err) throw err
+    client.query('SELECT id, name from users WHERE email = $1 and password = $2', [email, password],(err, result) => {
+      done();
+      if(result.rows[0]){
+        /*req.session.user = user;
+        req.session.userid = id;*/
+        console.log('results ', result.rows[0])
+        res.json({success: true});
+      }
+      else{
+        res.json({success: false});
+      }
+
+    });
+  });
+});
+
+app.post('/logout', (req, res)=>{
+  if(req.session.user){
+    delete req.session.user;
+    res.json({success: true});
+  }
+  else{
+    res.json({success: false});
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
